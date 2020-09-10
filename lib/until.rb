33e439a671465793a9a025e63ea4534ea1bd708c
Taylor Booth <taylorkbooth@gmail.com>
2020-09-10 23:08:35 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,12 +1,12 @@
 def using_until
   levitation_force = 0
  
-until levitation_force < 7
+  until levitation_force < 7
   levitation_force += 1
-  puts " #{num_of_hotdogs_eaten}Wingardium Leviosa"
-end
+    puts " #{num_of_hotdogs_eaten} Wingardium Leviosa"
+
  
-puts "You now have #{num_of_hotdogs_eaten} Wingardium Leviosa"
+    puts "You now have #{num_of_hotdogs_eaten} Wingardium Leviosa"
   end
 end
 
